61f499232fe8ee195e98313f7bbaf62ef9b70910
notaturkey <tommytocool5@gmail.com>
2021-07-20 21:57:24 -0400

    deploying this v

--- src/firstBug.js
+++ src/firstBug.js
@@ -21,7 +21,7 @@ export function startFirstBug(scene) {
     scene.okButton = new helper.AttackButton(300,750,'Ok', scene, () => {
     if (!scene.isTyping && !secondPrompt) {
           helper.textBoxDestroy(scene);
-            helper.textBox(scene,"Which skill should thomas use to defeat this bug?");
+            helper.textBox(scene,"Which skill should Thomas use to defeat this bug?");
             secondPrompt = true;
         }
         else if (!scene.isTyping){
@@ -35,19 +35,19 @@ export function startFirstBug(scene) {
 };
 
 function skills(scene){
-    pybutton = new helper.AttackButton(70,700,'Python', scene, () => {
+    pybutton = new helper.AttackButton(70,650,'Python', scene, () => {
         if (!scene.isTyping) {
             attack(scene);
         }
     }, ()=>{});
 
-    jbutton = new helper.AttackButton(180,700,'JavaScript', scene, () => {
+    jbutton = new helper.AttackButton(180,650,'JavaScript', scene, () => {
         if (!scene.isTyping) {
             attack(scene);
         }
     }, ()=>{});
 
-    pobutton = new helper.AttackButton(320,700,'Powershell', scene, () => {
+    pobutton = new helper.AttackButton(320,650,'Powershell', scene, () => {
         if (!scene.isTyping) {
             attack(scene);
         }
@@ -85,7 +85,7 @@ function wasAttacked1(scene) {
     scene.okButton = new helper.AttackButton(300,750,'Ok', scene, () => {
     if (!scene.isTyping && !secondPrompt) {
           helper.textBoxDestroy(scene);
-            helper.textBox(scene,"Which skill should thomas use to defeat this bug?");
+            helper.textBox(scene,"What will Thomas use in his arsenol next?");
             secondPrompt = true;
         }
         else if (!scene.isTyping){
@@ -99,19 +99,19 @@ function wasAttacked1(scene) {
 }
 
 function skills2(scene){
-    pybutton = new helper.AttackButton(70,700,'Java', scene, () => {
+    pybutton = new helper.AttackButton(70,650,'Java', scene, () => {
         if (!scene.isTyping) {
             attack2(scene);
         }
     }, ()=>{});
 
-    jbutton = new helper.AttackButton(180,700,'Jenkins', scene, () => {
+    jbutton = new helper.AttackButton(180,650,'Jenkins', scene, () => {
         if (!scene.isTyping) {
             attack2(scene);
         }
     }, ()=>{});
 
-    pobutton = new helper.AttackButton(320,700,'Junit', scene, () => {
+    pobutton = new helper.AttackButton(320,650,'Junit', scene, () => {
         if (!scene.isTyping) {
             attack2(scene);
         }
@@ -145,12 +145,12 @@ function wasAttacked2(scene) {
     scene.enemyStats.destroy();
     scene.enemyStats = scene.add.text(260, 370, 'HP:'+scene.enemyHealth+'/100\nLevel:0\nStatus:\n---Poorly Drawn\nDescription:\n---This guy pops\n---up alot.', {font: 'bold 10px Arial', fill: 'white', align: 'left', wordWrap: { width: 275, useAdvancedWrap: true } });
 
-    helper.textBox(scene,"Its a good thing Thomas learned those skills at Travelport!");
+    helper.textBox(scene,"Thomas praises the skills he gained at Travelport!");
     var secondPrompt = false;
     scene.okButton = new helper.AttackButton(300,750,'Ok', scene, () => {
     if (!scene.isTyping && !secondPrompt) {
           helper.textBoxDestroy(scene);
-            helper.textBox(scene,"Which skill should thomas use to defeat this bug?");
+            helper.textBox(scene,"Which skill should Thomas use next?");
             secondPrompt = true;
         }
         else if (!scene.isTyping){
@@ -169,19 +169,19 @@ function skills3(scene){
         }
     }, ()=>{});
 
-    button2 = new helper.AttackButton(300,700,'node.js with electron', scene, () => {
+    button2 = new helper.AttackButton(270,700,'node.js with electron', scene, () => {
         if (!scene.isTyping) {
             attack3(scene);
         }
     }, ()=>{});
 
-    button3 = new helper.AttackButton(70,600,'MongoDB', scene, () => {
+    button3 = new helper.AttackButton(70,650,'MongoDB', scene, () => {
         if (!scene.isTyping) {
             attack3(scene);
         }
     }, ()=>{});
 
-    button4 = new helper.AttackButton(300,600,'AWS', scene, () => {
+    button4 = new helper.AttackButton(200,650,'AWS', scene, () => {
         if (!scene.isTyping) {
             attack3(scene);
         }
@@ -215,12 +215,12 @@ function wasAttacked3(scene) {
     scene.enemyStats.destroy();
     scene.enemyStats = scene.add.text(260, 370, 'HP:'+scene.enemyHealth+'/100\nLevel:0\nStatus:\n---Poorly Drawn\nDescription:\n---This guy pops\n---up alot.', {font: 'bold 10px Arial', fill: 'white', align: 'left', wordWrap: { width: 275, useAdvancedWrap: true } });
 
-    helper.textBox(scene,"Its a good thing Thomas learned those skills at Samtec Microelectronics!");
+    helper.textBox(scene,"Samtec Microelectronics really left Thomas well prepared!");
     var secondPrompt = false;
     scene.okButton = new helper.AttackButton(300,750,'Ok', scene, () => {
     if (!scene.isTyping && !secondPrompt) {
           helper.textBoxDestroy(scene);
-            helper.textBox(scene,"Which skill should thomas use to defeat this bug?");
+            helper.textBox(scene,"What skill should Thomas use now?");
             secondPrompt = true;
         }
         else if (!scene.isTyping){
@@ -252,7 +252,7 @@ function skills4(scene){
         }
     }, ()=>{});
 
-    button4 = new helper.AttackButton(180,600,'Bash', scene, () => {
+    button4 = new helper.AttackButton(180,650,'Bash', scene, () => {
         if (!scene.isTyping) {
             attack4(scene);
         }
@@ -293,7 +293,7 @@ function wasAttacked4(scene) {
         case 0:
             prompt += 1;
             helper.textBoxDestroy(scene);
-            helper.textBox(scene,"Its a good thing Thomas learned those skills his internship at NASA and his fulltime position following the internship!");
+            helper.textBox(scene,"Thomas's internship at NASA and his fulltime position following the internship really packs a punch!");
             // code block
             break;
         case 1:
